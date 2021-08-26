8c255c917fa4c5a0c50cef5ffc60776d9a684e73
Zi Ching <zichingtan18@gmail.com>
2021-08-26 14:23:38 +0800

    adding function on main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 # include <iostream>
+# include <cstdlib>
 using namespace std;
+
+void welcomePage()
+{
+    cout << "\n\n" << endl;
+    cout << "\t\tWelcome to" << endl;
+    cout << "\t    Hot Babes Presents" << endl;
+    cout << "\tVOLUNTEER MANAGEMENT SYSTEM" << endl;
+    cout << "\n\n" << endl;
+}
 
 int main()
 {
+    welcomePage();
+    system("pause");
     return 0;
 }
